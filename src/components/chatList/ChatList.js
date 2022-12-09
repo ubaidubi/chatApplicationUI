1ec5321116cb8b5d6c2513a23f2befa83454f445
ubaidubi <120138043+ubaidubi@users.noreply.github.com>
2022-12-08 22:35:44 -0800

    Add files via upload

---
+++ src/components/chatList/ChatList.js
@@ -0,0 +1,131 @@
+import React, { Component } from "react";
+import "./chatList.css";
+import ChatListItems from "./ChatListItems";
+
+export default class ChatList extends Component {
+  allChatUsers = [
+    {
+      image:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTA78Na63ws7B7EAWYgTr9BxhX_Z8oLa1nvOA&usqp=CAU",
+      id: 1,
+      name: "Ubaid Khan",
+      active: true,
+      isOnline: true,
+    },
+    {
+      image:
+        "https://pbs.twimg.com/profile_images/1055263632861343745/vIqzOHXj.jpg",
+      id: 2,
+      name: "Abdullah Mughal",
+      active: false,
+      isOnline: false,
+    },
+    {
+      image:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTQEZrATmgHOi5ls0YCCQBTkocia_atSw0X-Q&usqp=CAU",
+      id: 3,
+      name: "Sabir Hussain",
+      active: false,
+      isOnline: false,
+    },
+    {
+      image:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRZ6tM7Nj72bWjr_8IQ37Apr2lJup_pxX_uZA&usqp=CAU",
+      id: 4,
+      name: "Talha Amjad",
+      active: false,
+      isOnline: true,
+    },
+    {
+      image:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRJo1MiPQp3IIdp54vvRDXlhbqlhXW9v1v6kw&usqp=CAU",
+      id: 5,
+      name: "Shehraz Hassan",
+      active: false,
+      isOnline: false,
+    },
+    {
+      image:
+        "https://huber.ghostpool.com/wp-content/uploads/avatars/3/596dfc2058143-bpfull.png",
+      id: 6,
+      name: "Subhan Ahmad",
+      active: false,
+      isOnline: true,
+    },
+    {
+      image:
+        "https://www.paintingcontest.org/components/com_djclassifieds/assets/images/default_profile.png",
+      id: 7,
+      name: "Shahid Hussain",
+      active: false,
+      isOnline: true,
+    },
+    {
+      image:
+        "https://auraqatar.com/projects/Anakalabel/media//vesbrand/designer4.jpg",
+      id: 8,
+      name: "Shahzaib Hassan",
+      active: false,
+      isOnline: false,
+    },
+    {
+      image:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSM6p4C6imkewkCDW-9QrpV-MMAhOC7GnJcIQ&usqp=CAU",
+      id: 9,
+      name: "Hafeez Ullah",
+      active: false,
+      isOnline: true,
+    },
+    {
+      image: "https://pbs.twimg.com/profile_images/770394499/female.png",
+      id: 10,
+      name: "Subhan Mubeen",
+      active: false,
+      isOnline: true,
+    },
+  ];
+  constructor(props) {
+    super(props);
+    this.state = {
+      allChats: this.allChatUsers,
+    };
+  }
+  render() {
+    return (
+      <div className="main__chatlist">
+        <button className="btn">
+          <i className="fa fa-plus"></i>
+          <span>New conversation</span>
+        </button>
+        <div className="chatlist__heading">
+          <h2>Chats</h2>
+          <button className="btn-nobg">
+            <i className="fa fa-ellipsis-h"></i>
+          </button>
+        </div>
+        <div className="chatList__search">
+          <div className="search_wrap">
+            <input type="text" placeholder="Search Here" required />
+            <button className="search-btn">
+              <i className="fa fa-search"></i>
+            </button>
+          </div>
+        </div>
+        <div className="chatlist__items">
+          {this.state.allChats.map((item, index) => {
+            return (
+              <ChatListItems
+                name={item.name}
+                key={item.id}
+                animationDelay={index + 1}
+                active={item.active ? "active" : ""}
+                isOnline={item.isOnline ? "active" : ""}
+                image={item.image}
+              />
+            );
+          })}
+        </div>
+      </div>
+    );
+  }
+}
